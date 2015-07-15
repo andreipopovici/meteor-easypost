a6b493b2fec5b7f6c5e8955052029efc813d8886
Andrei Popovici <me@andreipopovici.net>
2015-07-15 09:50:38 +0300

    Update package.js

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'adyus:easypost',
-  version: '2.0.5_4',
+  version: '2.0.7',
   // Brief, one-line summary of the package.
   summary: 'Meteor version of node-easypost',
   // URL to the Git repository containing the source code for this package.
